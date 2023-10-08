cab9ea9426b589e5f089748fad21a6e9c0814d21
OgnennuyVladuka <146809006+OgnennuyVladuka@users.noreply.github.com>
2023-10-08 22:27:20 +0300

    Add files via upload

---
+++ z5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace z5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение без знаков препинаний:");
+            string s = Console.ReadLine();
+            string[] names = s.Split(' ');
+            int dlin = names.Length;
+            int indexofmax = 0;
+            for (int i = 0; i < dlin; i++)
+            {
+                if (names[i].Length > names[indexofmax].Length)
+                {
+                    indexofmax = i;
+                }
+            }
+            Console.WriteLine("Самое длинное слово:\r\n" +
+                (names[indexofmax]));
+        }
+    }
+}
